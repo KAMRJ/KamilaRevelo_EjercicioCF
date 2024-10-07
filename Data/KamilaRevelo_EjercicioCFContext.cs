a7914bc6669f8dc301fdc512c9e2fd1d8ac70443
KAMRJ <kamyrevelo194@gmail.com>
2024-10-07 11:16:13 -0500

    Agregar archivos de proyecto.

---
+++ Data/KamilaRevelo_EjercicioCFContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using KamilaRevelo_EjercicioCF.Models;
+
+    public class KamilaRevelo_EjercicioCFContext : DbContext
+    {
+        public KamilaRevelo_EjercicioCFContext (DbContextOptions<KamilaRevelo_EjercicioCFContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<KamilaRevelo_EjercicioCF.Models.Burger> Burger { get; set; } = default!;
+    }
